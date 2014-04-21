e01509fd79ff1f251a091f5b714d85ce2879d780
alcexhim <alcexhim@gmail.com>
2014-04-21 07:46:36 -0400

    Added ErrorException event handler

--- ServerSide/WebFX.inc.php
+++ ServerSide/WebFX.inc.php
@@ -1,6 +1,12 @@
 <?php
 	namespace WebFX;
 	
+	function wfx_exception_error_handler($errno, $errstr, $errfile, $errline)
+	{
+		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+	}
+	set_error_handler("WebFX\\wfx_exception_error_handler");
+	
 	use WebFX\Pages\ErrorPage;
 
 	class ErrorEventArgs
