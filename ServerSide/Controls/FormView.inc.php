61175af520577fe4f90b503de5b28586bc0779bc
alcexhim <alcexhim@gmail.com>
2014-11-25 23:01:36 -0500

    Various enhancements to FormView, and ensure child elements are parsed

--- ServerSide/Controls/FormView.inc.php
+++ ServerSide/Controls/FormView.inc.php
@@ -1,9 +1,17 @@
 <?php
 	namespace WebFX\Controls;
+	
+	use WebFX\HTMLControls\HTMLControlInput;
+	use WebFX\HTMLControls\HTMLControlInputType;
 
 	class FormView extends \WebFX\WebControl
 	{
 		public $Items;
+		
+		public function __construct()
+		{
+			$this->ParseChildElements = true;
+		}
 		
 		protected function RenderContent()
 		{
@@ -16,10 +24,17 @@
 					case 'WebFX\Controls\FormViewItemText':
 					{
 						echo("Text");
+						break;
+					}
+					case 'WebFX\Controls\FormViewItemPassword':
+					{
+						echo("Password");
+						break;
 					}
 					case 'WebFX\Controls\FormViewItemMemo':
 					{
 						echo("Memo");
+						break;
 					}
 				}
 				if ($item->Required) echo(" Required");
@@ -29,11 +44,22 @@
 				{
 					case 'WebFX\Controls\FormViewItemText':
 					{
-						echo("<input type=\"text\"");
-						echo(" id=\"" . $item->ID . "\"");
-						echo(" name=\"" . $item->Name . "\"");
-						echo(" value=\"" . $item->DefaultValue . "\"");
-						echo(" />");
+						$elem = new HTMLControlInput();
+						$elem->ID = $item->ID;
+						$elem->Type = HTMLControlInputType::Text;
+						$elem->Name = $item->Name;
+						$elem->Value = $item->DefaultValue;
+						$elem->Render();
+						break;
+					}
+					case 'WebFX\Controls\FormViewItemPassword':
+					{
+						$elem = new HTMLControlInput();
+						$elem->ID = $item->ID;
+						$elem->Type = HTMLControlInputType::Password;
+						$elem->Name = $item->Name;
+						$elem->Value = $item->DefaultValue;
+						$elem->Render();
 						break;
 					}
 					case 'WebFX\Controls\FormViewItemMemo':
@@ -53,6 +79,10 @@
 						echo($item->DefaultValue);
 						echo("</textarea>");
 						break;
+					}
+					default:
+					{
+						echo("<!-- class not implemented: " . get_class($item) . " -->");
 					}
 				}
 				echo("</div>");
@@ -84,6 +114,13 @@
 		}
 	}
 	class FormViewItemText extends FormViewItem
+	{
+		public function __construct($id, $name = null, $title = null, $defaultValue = null)
+		{
+			parent::__construct($id, $name, $title, $defaultValue);
+		}
+	}
+	class FormViewItemPassword extends FormViewItemText
 	{
 		public function __construct($id, $name = null, $title = null, $defaultValue = null)
 		{
