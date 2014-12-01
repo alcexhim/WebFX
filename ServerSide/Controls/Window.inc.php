a0115c576cb91ac3ed2dfc9b32d2a2f7bd1fb3ad
alcexhim <alcexhim@gmail.com>
2014-12-01 00:22:44 -0500

    Improved Window code

--- ServerSide/Controls/Window.inc.php
+++ ServerSide/Controls/Window.inc.php
@@ -1,7 +1,10 @@
 <?php
 	namespace WebFX\Controls;
 	use System;
+	
 	use WebFX\WebControl;
+	use WebFX\WebControlAttribute;
+	use WebFX\WebScript;
 	
 	use WebFX\HorizontalAlignment;
 	use WebFX\VerticalAlignment;
@@ -12,114 +15,96 @@
 		
 		private $HasButtons;
 		
-		public function __construct($id, $title = null)
+		public function __construct()
 		{
-			parent::__construct($id);
-			$this->Title = $title;
+			parent::__construct();
 			$this->HasButtons = false;
+			$this->TagName = "div";
+			$this->ClassList[] = "Window";
+		}
+		
+		protected function OnInitialize()
+		{
+			$parent = $this->FindParentPage();
+			if ($parent != null) $parent->Scripts[] = new WebScript("$(WebFXStaticPath)/Scripts/Controls/Window.js");
+		}
+		
+		protected function RenderBeginTag()
+		{
+			switch($this->HorizontalAlignment)
+			{
+				case HorizontalAlignment::Left:
+				{
+					$this->Attributes[] = new WebControlAttribute("data-horizontal-alignment", "left");
+					break;
+				}
+				case HorizontalAlignment::Center:
+				{
+					$this->Attributes[] = new WebControlAttribute("data-horizontal-alignment", "center");
+					break;
+				}
+				case HorizontalAlignment::Right:
+				{
+					$this->Attributes[] = new WebControlAttribute("data-horizontal-alignment", "right");
+					break;
+				}
+			}
+			switch($this->VerticalAlignment)
+			{
+				case VerticalAlignment::Top:
+				{
+					$this->Attributes[] = new WebControlAttribute("data-vertical-alignment", "top");
+					break;
+				}
+				case VerticalAlignment::Middle:
+				{
+					$this->Attributes[] = new WebControlAttribute("data-vertical-alignment", "middle");
+					break;
+				}
+				case VerticalAlignment::Bottom:
+				{
+					$this->Attributes[] = new WebControlAttribute("data-vertical-alignment", "bottom");
+					break;
+				}
+			}
+			parent::RenderBeginTag();
 		}
 		
 		protected function BeforeContent()
 		{
-?>
-<div class="Window" id="Window_<?php echo($this->ID); ?>"<?php if (!($this->Visible)) { echo(" style=\"display: none;\""); } ?>>
-	<div class="TitleBar" id="Window_<?php echo($this->ID); ?>_TitleBar"><span class="Title" id="Window_<?php echo($this->ID); ?>_TitleBar_Title"><?php echo($this->Title); ?></span></div>
-	<div class="Content"<?php
-	if ($this->Width != null)
-	{
-		if (is_numeric($this->Width))
-		{
-			echo(" style=\"width: " . $this->Width . "px;\"");
-		}
-		else
-		{
-			echo(" style=\"width: " . $this->Width . ";\"");
-		}
-	} ?>>
-<?php
+			echo("<div class=\"TitleBar\"><span class=\"Title\">" . $this->Title . "</span></div>");
+			echo("<div class=\"Content\"");
+			if ($this->Width != null)
+			{
+				if (is_numeric($this->Width))
+				{
+					echo(" style=\"width: " . $this->Width . "px;\"");
+				}
+				else
+				{
+					echo(" style=\"width: " . $this->Width . ";\"");
+				}
+			}
+			echo(">");
 		}
 		
 		public function BeginButtons()
 		{
-?>
-	</div>
-	<div class="Buttons">
-<?php
+			echo("</div>");
+			echo("<div class=\"Buttons\">");
 		}
 		public function EndButtons()
 		{
-?>
-	</div>
-<?php
+			echo("</div>");
 			$this->HasButtons = true;
 		}
 		
 		protected function AfterContent()
 		{
-if (!$this->HasButtons)
-{
-?>
-	</div>
-<?php
-}
-?>
-</div>
-<script type="text/javascript">
-	var <?php echo($this->ID); ?> = new Window("<?php echo($this->ID); ?>");
-	<?php
-	switch($this->HorizontalAlignment)
-	{
-		case HorizontalAlignment::Left:
-		{
-			echo($this->ID . ".SetHorizontalAlignment(HorizontalAlignment.Left);");
-			break;
-		}
-		case HorizontalAlignment::Center:
-		{
-			echo($this->ID . ".SetHorizontalAlignment(HorizontalAlignment.Center);");
-			break;
-		}
-		case HorizontalAlignment::Right:
-		{
-			echo($this->ID . ".SetHorizontalAlignment(HorizontalAlignment.Right);");
-			break;
-		}
-		default:
-		{
-			echo("// Window: invalid value \"" . $this->HorizontalAlignment . "\" for \"HorizontalAlignment\"\n");
-			break;
-		}
-	}
-	switch($this->VerticalAlignment)
-	{
-		case VerticalAlignment::Top:
-		{
-			echo($this->ID . ".SetVerticalAlignment(VerticalAlignment.Top);");
-			break;
-		}
-		case VerticalAlignment::Middle:
-		{
-			echo($this->ID . ".SetVerticalAlignment(VerticalAlignment.Middle);");
-			break;
-		}
-		case VerticalAlignment::Bottom:
-		{
-			echo($this->ID . ".SetVerticalAlignment(VerticalAlignment.Bottom);");
-			break;
-		}
-		default:
-		{
-			echo("// Window: invalid value \"" . $this->VerticalAlignment . "\" for \"VerticalAlignment\"\n");
-			break;
-		}
-	}
-	if ($this->Top != null)
-	{
-		echo($this->ID . ".SetTop(" . $this->Top . ");");
-	}
-	?>
-</script>
-<?php
+			if (!$this->HasButtons)
+			{
+				echo("</div>");
+			}
 		}
 	}
 ?>
