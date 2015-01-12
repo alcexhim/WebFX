881394f67e1609018b39e2392557b71f3296cb99
alcexhim <alcexhim@gmail.com>
2015-01-12 00:39:14 -0500

    Implemented dynamic Menu

--- ClientSide/Scripts/Controls/Menu.js
+++ ClientSide/Scripts/Controls/Menu.js
@@ -1,3 +1,50 @@
+function ContextMenu()
+{
+	this.Items = [];
+	this.ParentElement = null;
+	this.Show = function(x, y)
+	{
+		if (this.ParentElement == null)
+		{
+			var elem = document.createElement("div");
+			elem.className = "Menu Popup";
+			
+			for (var i = 0; i < this.Items.length; i++)
+			{
+				var elem1 = document.createElement("a");
+				elem1.setAttribute("href", "#");
+				elem1.addEventListener("click", function(e)
+				{
+					this.MenuItem.Execute();
+				});
+				elem1.innerHTML = this.Items[i].Title;
+				elem1.MenuItem = this.Items[i];
+				
+				elem.appendChild(elem1);
+			}
+			
+			document.body.appendChild(elem);
+			this.ParentElement = elem;
+		}
+		this.ParentElement.className = "Menu Popup Visible";
+	};
+	this.Hide = function()
+	{
+		if (this.ParentElement == null) return;
+		this.ParentElement.className = "Menu Popup";
+	};
+}
+function MenuItemCommand(id, title, onclick)
+{
+	this.ID = id;
+	this.Title = title;
+	this.OnClientClick = onclick;
+	
+	this.Execute = function()
+	{
+		if (this.OnClientClick != null) this.OnClientClick();
+	};
+}
 function Menu(id)
 {
 	this.ID = id;
