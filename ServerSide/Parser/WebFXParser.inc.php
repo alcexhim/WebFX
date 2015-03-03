beab7c6921d8c4aed6384a3a861dc7a9ff805467
alcexhim <alcexhim@gmail.com>
2015-03-02 20:34:20 -0500

    Pull in the XHTML 1.1 DOCTYPE for HTML entity support

--- ServerSide/Parser/WebFXParser.inc.php
+++ ServerSide/Parser/WebFXParser.inc.php
@@ -317,6 +317,7 @@
 			if (!$this->UseCompatibleRenderingMode)
 			{
 				echo("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+				echo("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">");
 			}
 			else
 			{
