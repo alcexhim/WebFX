2e80bcb81e188957cd63fd93900792d2b4bff4af
alcexhim <alcexhim@gmail.com>
2015-03-03 16:18:47 -0500

    Use extensible Enumeration instead of boolean for http-equiv since there can be other metadata types

--- ServerSide/WebPageMetadata.inc.php
+++ ServerSide/WebPageMetadata.inc.php
@@ -1,17 +1,40 @@
 <?php
 	namespace WebFX;
+
+	/**
+	 * Provides an enumeration of predefined values for type of WebPage metadata.
+	 * @author Michael Becker
+	 */
+	abstract class WebPageMetadataType extends Enumeration
+	{
+		/**
+		 * The metadata is of type "name".
+		 * @var int 0
+		 */
+		const Name = 0;
+		/**
+		 * The metadata is of type "http-equiv".
+		 * @var int 1
+		 */
+		const HTTPEquivalent = 1;
+		/**
+		 * The metadata is of type "property".
+		 * @var int 2
+		 */
+		const Property = 2;
+	}
 	
     class WebPageMetadata
     {
         public $Name;
         public $Content;
-        public $IsHTTPEquivalent;
+        public $Type;
         
-        public function __construct($name, $content = "", $isHTTPEquivalent = false)
+        public function __construct($name, $content = "", $type = WebPageMetadataType::Name)
         {
             $this->Name = $name;
             $this->Content = $content;
-            $this->IsHTTPEquivalent = $isHTTPEquivalent;
+            $this->Type = $type;
         }
     }
 ?>
